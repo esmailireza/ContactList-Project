17ba54924394ce8220c0ace5f6d70b874b8f84bb
esmailireza <esmailireza20@gmail.com>
2022-01-03 23:58:46 +0330

    maped on states

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,13 +1,21 @@
 import styles from "./form.module.css";
-const ContactList = ({ name, email }) => {
+const ContactList = ({ state }) => {
   return (
-    <ul className={styles.ul}>
-      <li>
-        <p>{name}</p>
-        <span>{email}</span>
-        <hr />
-      </li>
-    </ul>
+    <>
+      {state.map((co) => {
+        console.log(co);
+        console.log("contact:", co.name);
+        return (
+          <ul className={styles.ul}>
+            <li key={co.id}>
+              <p>{co.name}</p>
+              <span>{co.email}</span>
+              <hr />
+            </li>
+          </ul>
+        );
+      })}
+    </>
   );
 };
 
