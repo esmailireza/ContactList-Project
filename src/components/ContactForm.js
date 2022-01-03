dfcf747e9972fd058c58ba51fe6658d38394dfec
esmailireza <esmailireza20@gmail.com>
2022-01-03 23:57:43 +0330

    updated eventHandler states

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,47 +1,54 @@
 import { useState } from "react";
 import styles from "../components/form.module.css";
+import ContactList from "./ContactList";
+
 const ContactForm = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
+  const [state, setState] = useState([]);
+  //console.log(state);
+
   const submitFormHandler = (e) => {
     e.preventDefault();
-    //console.log("event:", e);
-    console.log("name:", name);
-    console.log("email:", email);
+    console.log(state);
+    setState([...state, { name: state.name, email: state.email, id: 1 }]);
   };
 
   const nameHandler = (e) => {
-    //console.log(e.target.value);
-    setName(e.target.value);
+    console.log(e.target.value);
+    setState({ name: e.target.value });
   };
   const emailHandler = (e) => {
-    //console.log(e.target.value);
-    setEmail(e.target.value);
+    console.log(e.target.value);
+    setState({ email: e.target.value });
   };
   return (
     <main className={styles.main}>
       <form className={styles.form}>
         <h2>Add Contact</h2>
-        <label for="input_name">Name</label>
+        <label htmlFor="input_name">Name</label>
         <input
           type="text"
           name="name"
           id="input_name"
           onChange={nameHandler}
-          value={name}
+          value={state.name}
         />
-        <label>Email</label>
+        <label htmlFor="input_email">Email</label>
         <input
           type="email"
           name="email"
           id="input_email"
           onChange={emailHandler}
-          value={email}
+          value={state.email}
         />
         <button className={styles.btn} onClick={submitFormHandler}>
           Add
         </button>
       </form>
+      <ContactList
+        /* submitFormHandler={submitFormHandler}
+        setState={setState} */
+        state={state}
+      />
     </main>
   );
 };
