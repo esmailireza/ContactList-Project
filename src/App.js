589f753a6f7bcac6a4c26f2053b646e6be968594
esmailireza <esmailireza20@gmail.com>
2022-01-04 23:51:15 +0330

    declared global state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import { useState } from "react/cjs/react.development";
 import ContactForm from "./components/ContactForm";
 import Header from "./components/Header";
 
 function App() {
+  const [contacts, setContacts] = useState([]);
+  //todos?
   return (
     <>
       <Header />
-      <ContactForm />
+      <ContactForm contacts={contacts} setContacts={setContacts} />
     </>
   );
 }
