4cfaf04e5bfb0152e62c91b1604ef9a88efc7fe3
esmailireza <esmailireza20@gmail.com>
2022-01-05 18:13:26 +0330

    created delete handler to delete button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import { useState } from "react/cjs/react.development";
 import ContactForm from "./components/ContactForm";
+import ContactList from "./components/ContactList";
 import Header from "./components/Header";
-
 function App() {
   const [contacts, setContacts] = useState([]);
+  const deleteHandler = (id) => {
+    /* console.log("deleted:", e.target); */
+    console.log("id:", id);
+    const filteredContact = contacts.filter((c) => c.id !== id);
+    setContacts(filteredContact);
+  };
   //todos?
   return (
     <>
       <Header />
-      <ContactForm contacts={contacts} setContacts={setContacts} />
+      <ContactForm
+        contacts={contacts}
+        setContacts={setContacts}
+        onDelete={deleteHandler}
+      />
+      <ContactList contacts={contacts} onDelete={deleteHandler} />
     </>
   );
 }
