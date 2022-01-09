c560d1b0911c70056fe39523a3def6aec97ad842
esmailireza <esmailireza20@gmail.com>
2022-01-09 21:41:45 +0330

    styles to button add and delete and so on

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,10 +1,16 @@
 import { Link } from "react-router-dom";
 import Contact from "./Contact";
-
+import styles from "../components/form.module.css";
+console.log(styles);
 const ContactList = ({ contacts, onDelete }) => {
   return (
     <>
-      <Link to="/add">Form</Link>
+      <div className={styles.nav}>
+        <h3>contacts</h3>
+        <Link to="/add" className={styles.buttonAdd}>
+          Add
+        </Link>
+      </div>
       {contacts.map((contact) => {
         return <Contact onDelete={onDelete} contact={contact} />;
       })}
